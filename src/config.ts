f075386067c7ea0e53d1fafed6c656a4f8b9ee14
ArtemKohanenko <kartem0255@gmail.com>
2025-06-22 22:16:47 +0600

    update config

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 const envSchema = z.object({
     PORT: z.string().default("8080"),
-    TOKEN: z.string(),
+    TOKEN: z.string().default("87654321"),  // TODO: Fix that
 });
 
 export const config = envSchema.parse({
